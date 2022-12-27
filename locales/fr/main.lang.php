b2c7b7efc8d5eddab8018c14ee5f4e18f418efb0
franck <carnet.franck.paul@gmail.com>
2022-12-27 14:24:46 +0100

    Update locales and add minified versions

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -1,62 +1,71 @@
 <?php
-$GLOBALS['__l10n']['Smilies Editor'] = 'Éditeur d\'émoticônes';
-$GLOBALS['__l10n']['Toolbar'] = 'Barre d\'outils';
-$GLOBALS['__l10n']['Display smilies on toolbar'] = 'Afficher les émoticônes sur la barre d\'outils';
-$GLOBALS['__l10n']['Smilies'] = 'Émoticônes';
-$GLOBALS['__l10n']['Configuration file is not writable.'] = 'Fichier de configuration non modifiable.';
-$GLOBALS['__l10n']['Unable to write file %s. Please check your theme file and folders permissions.'] = 'Impossible d\'écrire le fichier %s. Veuillez vérifier les permissions du fichier et répertoires de votre thème.';
-$GLOBALS['__l10n']['Uploaded image is too big (height or width > 24px).'] = 'L\'image envoyée est trop grande (hauteur ou largeur > 24px).';
-$GLOBALS['__l10n']['This file %s is not an image. It would be difficult to use it for a smiley.'] = 'Ce fichier %s n\'est pas une image. Ce sera difficile de l\'utiliser pour un émoticône.';
-$GLOBALS['__l10n']['Active theme does not have required subfolder <code>%s</code>.'] = 'Le thème actif ne contient pas le sous-répertoire obligatoire <code>%s</code>.';
-$GLOBALS['__l10n']['Unable to create subfolder %s in your theme. Please check your folder permissions.'] = 'Impossible de créer le sous-répertoire %s dans votre thème. Veuillez vérifier les permissions du répertoire.';
-$GLOBALS['__l10n']['Empty smilies zip file.'] = 'Fichier zip d\'émoticônes vide.';
-$GLOBALS['__l10n']['The zip file does not appear to be a valid Dotclear smilies package.'] = 'The fichier zip n\'est pas un fichier d\'émoticônes Dotclear valide.';
-$GLOBALS['__l10n']['Definition'] = 'Définition';
-$GLOBALS['__l10n']['update'] = 'mettre à jour';
-$GLOBALS['__l10n']['delete'] = 'supprimer';
-$GLOBALS['__l10n']['display'] = 'afficher';
-$GLOBALS['__l10n']['hide'] = 'cacher';
-$GLOBALS['__l10n']['Configuration successfully updated.'] = 'Configuration mise à jour avec succès.';
-$GLOBALS['__l10n']['The subfolder has been successfully created'] = 'Le sous-répertoire a été créé avec succès.';
-$GLOBALS['__l10n']['Unused images have been successfully removed.'] = 'Les images non utilisées ont été supprimées avec succès.';
-$GLOBALS['__l10n']['The image <em>%s</em> has been successfully uploaded.'] = 'L\'image <em>%s</em> a été envoyée avec succès.';
-$GLOBALS['__l10n']['A smilies zip package has been successfully installed.'] = 'Un fichier zip contenant des émoticônes a été installé avec succès.';
-$GLOBALS['__l10n']['Smilies has been successfully removed.'] = 'Les émoticônes ont été supprimés avec succès.';
-$GLOBALS['__l10n']['Smilies has been successfully updated.'] = 'Les émoticônes ont été mis à jour avec succès.';
-$GLOBALS['__l10n']['Order of smilies has been successfully changed.'] = 'L\'ordre des émoticônes a été mis à jour avec succès.';
-$GLOBALS['__l10n']['These selected smilies are now hidden on toolbar.'] = 'Ces émoticônes sont désormais cachés dans la barre d\'outils.';
-$GLOBALS['__l10n']['These selected smilies are now displayed on toolbar'] = 'Ces émoticônes sont désormais visibles dans la barre d\'outils.';
-$GLOBALS['__l10n']['A new smiley has been successfully created'] = 'Un nouvel émoticône a été créé avec succès.';
-$GLOBALS['__l10n']['Are you sure you want to remove these %s ?'] = 'Êtes-vous certain que vous voulez supprimer ces %s ?';
-$GLOBALS['__l10n']['Your <a href="blog_theme.php">current theme</a> on this blog is "%s".'] = 'Le <a href="blog_theme.php">thème utilisé</a> actuellement sur votre blog est "%s".';
-$GLOBALS['__l10n']['No defined smiley yet.'] = 'Aucun émoticône défini pour le moment.';
-$GLOBALS['__l10n']['Configuration'] = 'Configuration';
-$GLOBALS['__l10n']['Show toolbar smilies in comments form'] = 'Afficher la barre d\'outils des émoticônes dans le formulaire des commentaires';
-$GLOBALS['__l10n']['Show images on preview'] = 'Afficher les images en prévisualisation';
-$GLOBALS['__l10n']['Comments form label:'] = 'Libellé du formulaire des commentaires';
-$GLOBALS['__l10n']['Don\'t forget to <a href="%s">display smilies</a> on your blog configuration.'] = 'N\'oubliez pas d\'<a href="%s">afficher les émoticônes</a> dans la configuration de votre blog.';
-$GLOBALS['__l10n']['Save'] = 'Enregistrer';
-$GLOBALS['__l10n']['Smilies set'] = 'Jeu d\'émoticônes';
-$GLOBALS['__l10n']['Code'] = 'Code';
-$GLOBALS['__l10n']['Image'] = 'Image';
-$GLOBALS['__l10n']['Filename'] = 'Nom du fichier';
-$GLOBALS['__l10n']['displayed'] = 'visible';
-$GLOBALS['__l10n']['undisplayed'] = 'caché';
-$GLOBALS['__l10n']['Selected smilies action:'] = 'Action pour les émoticônes sélectionnés :';
-$GLOBALS['__l10n']['Ok'] = 'Valider';
-$GLOBALS['__l10n']['Save order'] = 'Enregistrer l\'ordre';
-$GLOBALS['__l10n']['Initialize'] = 'Initialiser';
-$GLOBALS['__l10n']['New smiley'] = 'Nouvel émoticône';
-$GLOBALS['__l10n']['Required field'] = 'Champ obligatoire';
-$GLOBALS['__l10n']['Image:'] = 'Image :';
-$GLOBALS['__l10n']['Code:'] = 'Code :';
-$GLOBALS['__l10n']['Create'] = 'Créer';
-$GLOBALS['__l10n']['New image'] = 'Nouvelle image';
-$GLOBALS['__l10n']['Choose a file:'] = 'Choisissez un fichier :';
-$GLOBALS['__l10n']['Maximum size %s'] = 'Taille maximale %s';
-$GLOBALS['__l10n']['Send'] = 'Envoyer';
-$GLOBALS['__l10n']['Unused smilies'] = 'Images non utilisées';
-$GLOBALS['__l10n']['Delete'] = 'Supprimer';
-$GLOBALS['__l10n']['Download the smilies directory as a zip file'] = 'Télécharger le répertoire des émoticônes dans un fichier zip';
-$GLOBALS['__l10n']['Smilies toolbar'] = 'B';
-?>
+/**
+ * @package Dotclear
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
+ */
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+l10n::$locales['Smilies Editor'] = 'Éditeur d\'émoticônes';
+l10n::$locales['Toolbar'] = 'Barre d\'outils';
+l10n::$locales['Display smilies on toolbar'] = 'Afficher les émoticônes sur la barre d\'outils';
+l10n::$locales['Smilies'] = 'Émoticônes';
+l10n::$locales['Configuration file is not writable.'] = 'Fichier de configuration non modifiable.';
+l10n::$locales['Unable to write file %s. Please check your theme file and folders permissions.'] = 'Impossible d\'écrire le fichier %s. Veuillez vérifier les permissions du fichier et répertoires de votre thème.';
+l10n::$locales['Uploaded image is too big (height or width > 24px).'] = 'L\'image envoyée est trop grande (hauteur ou largeur > 24px).';
+l10n::$locales['This file %s is not an image. It would be difficult to use it for a smiley.'] = 'Ce fichier %s n\'est pas une image. Ce sera difficile de l\'utiliser pour un émoticône.';
+l10n::$locales['Active theme does not have required subfolder <code>%s</code>.'] = 'Le thème actif ne contient pas le sous-répertoire obligatoire <code>%s</code>.';
+l10n::$locales['Unable to create subfolder %s in your theme. Please check your folder permissions.'] = 'Impossible de créer le sous-répertoire %s dans votre thème. Veuillez vérifier les permissions du répertoire.';
+l10n::$locales['Empty smilies zip file.'] = 'Fichier zip d\'émoticônes vide.';
+l10n::$locales['The zip file does not appear to be a valid Dotclear smilies package.'] = 'The fichier zip n\'est pas un fichier d\'émoticônes Dotclear valide.';
+l10n::$locales['Definition'] = 'Définition';
+l10n::$locales['update'] = 'mettre à jour';
+l10n::$locales['delete'] = 'supprimer';
+l10n::$locales['display'] = 'afficher';
+l10n::$locales['hide'] = 'cacher';
+l10n::$locales['Configuration successfully updated.'] = 'Configuration mise à jour avec succès.';
+l10n::$locales['The subfolder has been successfully created'] = 'Le sous-répertoire a été créé avec succès.';
+l10n::$locales['Unused images have been successfully removed.'] = 'Les images non utilisées ont été supprimées avec succès.';
+l10n::$locales['The image <em>%s</em> has been successfully uploaded.'] = 'L\'image <em>%s</em> a été envoyée avec succès.';
+l10n::$locales['A smilies zip package has been successfully installed.'] = 'Un fichier zip contenant des émoticônes a été installé avec succès.';
+l10n::$locales['Smilies has been successfully removed.'] = 'Les émoticônes ont été supprimés avec succès.';
+l10n::$locales['Smilies has been successfully updated.'] = 'Les émoticônes ont été mis à jour avec succès.';
+l10n::$locales['Order of smilies has been successfully changed.'] = 'L\'ordre des émoticônes a été mis à jour avec succès.';
+l10n::$locales['These selected smilies are now hidden on toolbar.'] = 'Ces émoticônes sont désormais cachés dans la barre d\'outils.';
+l10n::$locales['These selected smilies are now displayed on toolbar'] = 'Ces émoticônes sont désormais visibles dans la barre d\'outils.';
+l10n::$locales['A new smiley has been successfully created'] = 'Un nouvel émoticône a été créé avec succès.';
+l10n::$locales['Are you sure you want to remove these %s ?'] = 'Êtes-vous certain que vous voulez supprimer ces %s ?';
+l10n::$locales['Your <a href="blog_theme.php">current theme</a> on this blog is "%s".'] = 'Le <a href="blog_theme.php">thème utilisé</a> actuellement sur votre blog est "%s".';
+l10n::$locales['No defined smiley yet.'] = 'Aucun émoticône défini pour le moment.';
+l10n::$locales['Configuration'] = 'Configuration';
+l10n::$locales['Show toolbar smilies in comments form'] = 'Afficher la barre d\'outils des émoticônes dans le formulaire des commentaires';
+l10n::$locales['Show images on preview'] = 'Afficher les images en prévisualisation';
+l10n::$locales['Comments form label:'] = 'Libellé du formulaire des commentaires';
+l10n::$locales['Don\'t forget to <a href="%s">display smilies</a> on your blog configuration.'] = 'N\'oubliez pas d\'<a href="%s">afficher les émoticônes</a> dans la configuration de votre blog.';
+l10n::$locales['Save'] = 'Enregistrer';
+l10n::$locales['Smilies set'] = 'Jeu d\'émoticônes';
+l10n::$locales['Code'] = 'Code';
+l10n::$locales['Image'] = 'Image';
+l10n::$locales['Filename'] = 'Nom du fichier';
+l10n::$locales['displayed'] = 'visible';
+l10n::$locales['undisplayed'] = 'caché';
+l10n::$locales['Selected smilies action:'] = 'Action pour les émoticônes sélectionnés :';
+l10n::$locales['Ok'] = 'Valider';
+l10n::$locales['Save order'] = 'Enregistrer l\'ordre';
+l10n::$locales['Initialize'] = 'Initialiser';
+l10n::$locales['New smiley'] = 'Nouvel émoticône';
+l10n::$locales['Required field'] = 'Champ obligatoire';
+l10n::$locales['Image:'] = 'Image :';
+l10n::$locales['Code:'] = 'Code :';
+l10n::$locales['Create'] = 'Créer';
+l10n::$locales['New image'] = 'Nouvelle image';
+l10n::$locales['Choose a file:'] = 'Choisissez un fichier :';
+l10n::$locales['Maximum size %s'] = 'Taille maximale %s';
+l10n::$locales['Send'] = 'Envoyer';
+l10n::$locales['Unused smilies'] = 'Images non utilisées';
+l10n::$locales['Delete'] = 'Supprimer';
+l10n::$locales['Download the smilies directory as a zip file'] = 'Télécharger le répertoire des émoticônes dans un fichier zip';
+l10n::$locales['Smilies toolbar'] = 'B';
