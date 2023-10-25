9691690b31041e34f12695550041b6a1d0a6b3ca
franck <carnet.franck.paul@gmail.com>
2023-10-25 12:13:34 +0200

    New release 4.0

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'smiliesEditor',
     'Smilies Editor',
     'Osku and contributors',
-    '3.0',
+    '4.0',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
